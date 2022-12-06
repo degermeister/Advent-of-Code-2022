10abfb05851a155bdd313f493c64f340afb9ac7a
degermeister <58044091+degermeister@users.noreply.github.com>
2022-12-06 16:25:05 +0100

    Update day06.py

--- day06.py
+++ day06.py
@@ -1,10 +1,14 @@
 
+start_of_packet_marker_length = 4
+start_of_message_marker_length = 14
+
+
 def find_marker(data, marker_length):
-    for index in xrange(len(datastream)):
-        quartet = datastream[index:index + marker_length]
+    for index in xrange(len(data)):
+        sub_section = data[index:index + marker_length]
         unique = 0
-        for character in quartet:
-            if quartet.count(character) > 1:
+        for character in sub_section:
+            if sub_section.count(character) > 1:
                 break
             else:
                 unique += 1
@@ -12,12 +16,9 @@ def find_marker(data, marker_length):
             break
     return index + marker_length
 
+
 if __name__ == '__main__':
     datastream = open('input06.txt', 'rb').readline().strip()
-    start_of_packet_marker_length = 4
-    start_of_message_marker_length = 14
-
     print 'Datastream:', datastream
-
     print 'start-of-packet marker:', find_marker(datastream, start_of_packet_marker_length)
     print 'start-of-message marker:', find_marker(datastream, start_of_message_marker_length)
